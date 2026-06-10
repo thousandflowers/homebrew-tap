dabdf32054bfb0ab12d95c4b4607f176801c7d6a
eugeniozamengopontrelli <eugeniozamengo@gmail.com>
2026-06-10 20:09:52 +0200

    chore: bump skillreaper to v0.1.1

--- Formula/skillreaper.rb
+++ Formula/skillreaper.rb
@@ -1,17 +1,27 @@
 class Skillreaper < Formula
   desc "Evidence-based pruning for your AI agent stack"
   homepage "https://github.com/thousandflowers/skillreaper"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_darwin_amd64.tar.gz"
-    sha256 "2ab10d7f24416f7d6d2d68b6e76686bb3c0888a9115d1ff5be52c52097ae9429"
+    sha256 "cfdd24ae5fed960ec88a5342a675337f991015ba5e92a0a437ee382842a8358f"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_darwin_arm64.tar.gz"
-    sha256 "9ce5525e3d5d397d45ee2db34f662b85520ea34dc27463406475f46561a46d86"
+    sha256 "4531b6b6451a0a6ed35999b71fa6d70774f3faa497fad3ce1cc213137a48cc63"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_linux_amd64.tar.gz"
+    sha256 "30ef63cb2b717969c47754c5e52c0f0f373450b3d226813f194f98423789adda"
+  end
+
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_linux_arm64.tar.gz"
+    sha256 "b2fdfdd67c00147ee93f2a87b56a132b24693ce03837d6f8ae49aba757f89b0f"
   end
 
   def install
