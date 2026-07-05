2725fc0ea5e826ca69a0a0a018ef740d639cfbc7
eugeniozamengopontrelli <eugeniozamengo@gmail.com>
2026-07-06 07:55:36 +0800

    fix: correct linux sha256 checksums for skillreaper v0.4.0
    Both linux stanzas carried the darwin_arm64 checksum, breaking
    brew install on all Linux hosts. Values now match checksums.txt
    of the v0.4.0 release.

--- Formula/skillreaper.rb
+++ Formula/skillreaper.rb
@@ -16,12 +16,12 @@ class Skillreaper < Formula
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_linux_amd64.tar.gz"
-    sha256 "7fa5ff9f4a6bf957b72406e00b43125132e4b28f94dda0aca6b72e2b22d6fbca"
+    sha256 "692b38c80614299b42eed53ade33c02e691aeada6d0a920c97ab334796cc9717"
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/thousandflowers/skillreaper/releases/download/v#{version}/skillreaper_linux_arm64.tar.gz"
-    sha256 "7fa5ff9f4a6bf957b72406e00b43125132e4b28f94dda0aca6b72e2b22d6fbca"
+    sha256 "9c83ecf84c5791119c18fd96293ada3378a172823e1c5f15d03ffdc6a40abd6f"
   end
 
   def install
