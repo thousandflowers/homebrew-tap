b983b10207f50c586791c5ca579e0dc133042971
Eugenio Zamengo Pontrelli <104426696+thousandflowers@users.noreply.github.com>
2026-08-22 22:14:09 +0200

    chore: qr-multi-imgs 1.6.0

--- Formula/qr-multi-imgs.rb
+++ Formula/qr-multi-imgs.rb
@@ -1,26 +1,26 @@
 class QrMultiImgs < Formula
   desc "TUI to scan QR codes from an images folder, organize and export results"
   homepage "https://github.com/thousandflowers/qr-multi-imgs"
-  version "1.4.1"
+  version "1.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/thousandflowers/qr-multi-imgs/releases/download/v#{version}/qr-multi-imgs_#{version}_macOS_arm64.tar.gz"
-      sha256 "cd995df4ee3516d291f21586f80e1392e5e5579e317e758f86b58e7e0576119d"
+      sha256 "5c32616ab54cf0386c98548b3fe68785a787d02edd3ec29900b62a607e6a6f55"
     else
       url "https://github.com/thousandflowers/qr-multi-imgs/releases/download/v#{version}/qr-multi-imgs_#{version}_macOS_amd64.tar.gz"
-      sha256 "6018d2584fc230ca2051375e10e5a04ba441b4a180cef9d9641dc5be4af9a944"
+      sha256 "aa209cef7272464891774161280c65720c7f5a668428b2fa6bd1bbb5ddd46613"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/thousandflowers/qr-multi-imgs/releases/download/v#{version}/qr-multi-imgs_#{version}_linux_arm64.tar.gz"
-      sha256 "3d2309df3acb59e7faa9db70d156d61c6c14f6497ff15a4debe7ebc177fa0a6b"
+      sha256 "1553c5abd0112b61f437cf6c9babe4f685984c0587ddb1e756c097ce096f5eca"
     else
       url "https://github.com/thousandflowers/qr-multi-imgs/releases/download/v#{version}/qr-multi-imgs_#{version}_linux_amd64.tar.gz"
-      sha256 "3fa5c7cf53987d7025146ce9f258cc856d30a6baa3d9e865bb82307eacc65acb"
+      sha256 "f9b43d0a42a9277cd6537dc68c4da740d708534162377bb30251a0dd598b72ec"
     end
   end
 
